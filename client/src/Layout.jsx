ca87b0a0a1f4e38760d26aa5c2ce2b16a52d02c1
Gonza-Butafuoco <butafuocogonzalo@gmail.com>
2024-06-18 00:23:09 -0300

    client side update

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -6,7 +6,7 @@ import Header from "./components/Header";
 
 export default function Layout(){
     return(
-        <div>
+        <div className="p-4 flex flex-col min-h-screen">
             <Header/>
             <Outlet/>
             
